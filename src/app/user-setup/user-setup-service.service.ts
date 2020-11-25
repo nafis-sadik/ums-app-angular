754729a20e9e69476db044c1c6d357818470297a
Mahmud <mmnabi.cse@gmail.com>
2020-11-25 10:15:45 +0600

    Sending object to .net core

--- src/app/user-setup/user-setup-service.service.ts
+++ src/app/user-setup/user-setup-service.service.ts
@@ -1,14 +1,28 @@
+import { map, catchError } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserSetupServiceService {
   private http: HttpClient;
+  private pagingParams = {
+    Page: 1,
+    PageSize: 10,
+    SearchString: '',
+  };
   constructor() {}
 
-  getUsers(): void {
-    this.http.get<any>(`https://localhost:44337/api/Users/GetAll/`);
+  getUsers(): Observable<any> {
+    return this.http
+      .get<any>(`https://localhost:44337/api/Users/GetAll/` + this.pagingParams)
+      .pipe(
+        map((response) => {
+          return response;
+        }),
+        catchError((err) => of(err))
+      );
   }
 }
